146d43a82ae0563b035e7b01db82f7f85ef32f5e
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-08-20 01:23:22 +0530

    infinite scroll added

--- src/components/Hotelcard/Hotelcard.jsx
+++ src/components/Hotelcard/Hotelcard.jsx
@@ -1,35 +1,27 @@
-export const HotelCard = ({items}) => {
-   console.log(items)
-    const { name, image, address, state, rating, price } = items;
-
-
+export const HotelCard = ({ items }) => {
+  console.log(items);
+  const { name, image, address, state, rating, price } = items;
 
   return (
-     items.map(value=> {
-        return(
     <div className="hotel-container d-flex dir-col">
       <div className="hotel-image-container">
-        <img
-          src={value.image}
-          alt={value.name}
-          className="hotel-image"
-        />
+        <img src={image} alt={name} className="hotel-image" />
       </div>
       <div className="hotel-description">
         <div className="d-flex dir-col ">
-          <h4>{value.address},{value.state}</h4>
-          <h5>{value.name}</h5>
+          <span>
+            {address},{state}
+          </span>
+          <span>{name}</span>
         </div>
       </div>
       <div className="hotel-footer d-flex  space-between">
-        <span>Rs.{value.price}/Night</span>
+        <span>Rs.{price}/Night</span>
         <div className="star">
-        <span className="material-icons-outlined">star</span>
-          <span>
-             {value.rating}
-          </span>
+          <span className="material-icons-outlined">star</span>
+          <span>{rating}</span>
         </div>
       </div>
-    </div>)})
+    </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,35 +1,72 @@
 import { Fragment, useEffect, useState } from "react";
 import axios from "axios";
-import { Navbar,HotelCard } from "../../components/index";
+import { Navbar, HotelCard } from "../../components/index";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export const Home=()=>{
+export const Home = () => {
+  const [hotels, setHotels] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [currentIndex, setCurrentIndex] = useState(12);
+  const [testData, setTestData] = useState([]);
 
-const [hotel,setHotel]= useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          "https://sore-blue-duckling-garb.cyclic.cloud/api/hotels"
+        );
+        setTestData(data);
+        setHotels(data ? data.slice(0, 12) : []);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
 
- useEffect(()=>{
-  (async ()=>{
-    try{
-        const {data}= await axios.get('https://sore-blue-duckling-garb.cyclic.cloud/api/hotels');
-        setHotel(data);
-    
+  const fetchMoreHotels = () => {
+    if (hotels.length >= testData.length) {
+      setHasMore(false);
+      return;
     }
-    catch(err){
-        console.log(err)
-    }
-  }
-  )()
- },[])
+    setTimeout(() => {
+      if (hotels && hotels.length > 0) {
+        setHotels(
+          hotels.concat(testData.slice(currentIndex, currentIndex + 12))
+        );
+        setCurrentIndex((prev) => prev + 12);
+      } else {
+        setHotels([]);
+      }
+    }, 700);
+  };
 
-
-
-    return(
-        <Fragment>
-            <Navbar/>
-            <div className="hotelcard-container">
-
-            <HotelCard key={hotel._id} items={hotel}/>
-            </div>
-            
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      <Navbar />
+      {hotels && hotels.length > 0 ? (
+        <InfiniteScroll
+          dataLength={hotels.length}
+          next={fetchMoreHotels}
+          hasMore={hasMore}
+          loader={
+            hotels.length > 0 && (
+              <span class="loader">
+               Loading....
+              </span>
+            )
+          }
+          endMessage={<p className="last-message">You have reached to the Last section &#x1F304; &#9749; </p>}
+        >
+          <main className="hotelcard-container gap-m">
+            {hotels &&
+              hotels.map((hotel) => (
+                <HotelCard key={hotel._id} items={hotel} />
+              ))}
+          </main>
+        </InfiniteScroll>
+      ) : (
+        <></>
+      )}
+    </Fragment>
+  );
+};
