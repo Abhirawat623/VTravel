6c25504b672d1cd6cdfe82544f412b847dbefeac
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-08-19 16:54:09 +0530

    dynamic data added by cylcic

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,33 @@
-import { Fragment } from "react"
-import { Navbar,HotelCard } from "../../components/index"
+import { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import { Navbar,HotelCard } from "../../components/index";
 
 export const Home=()=>{
+
+const [hotel,setHotel]= useState([]);
+
+ useEffect(()=>{
+  (async ()=>{
+    try{
+        const {data}= await axios.get('https://sore-blue-duckling-garb.cyclic.cloud/api/hotels');
+        setHotel(data);
+    
+    }
+    catch(err){
+        console.log(err)
+    }
+  }
+  )()
+ },[])
+
 
 
     return(
         <Fragment>
             <Navbar/>
             <div className="hotelcard-container">
-            <HotelCard/>
+
+            <HotelCard key={hotel._id} items={hotel}/>
             </div>
             
         </Fragment>
