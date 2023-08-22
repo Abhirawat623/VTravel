55496236643553ade309e6c6a635b34b0b516404
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-08-22 14:25:07 +0530

    hotels name added

--- src/components/singlehotelcard/singlehotelcard.jsx
+++ src/components/singlehotelcard/singlehotelcard.jsx
@@ -27,5 +27,26 @@ export const SingleHotelCard = () => {
     })();
   }, [id]);
 
-  return <HotelImages items={singleHotel} />;
+const {name,address,rating}= singleHotel;
+
+
+
+  return (
+  <div
+  className="single-hotel-components">
+
+    <div className="single-hotel-header">
+      <h2 className="single-hotel-name">{name}</h2>
+      <div className="rate-address d-flex dir-row  text-l">
+      <div className="single-hotel-star d-flex dir-row">
+          <span className="material-icons-outlined">star</span>
+          <span className="single-hotel-star-rating">{rating}</span>
+        </div>
+        <div className="single-hotel-address">{address}</div>
+      </div>
+    </div>
+    <div><HotelImages items={singleHotel} /></div>
+  
+  </div>
+  )
 };
