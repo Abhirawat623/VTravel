4498899de144ae464097664de46e4e5725260212
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-08-25 23:15:44 +0530

    added final price component

--- src/components/FinalPrice/FinalPrice.jsx
+++ src/components/FinalPrice/FinalPrice.jsx
@@ -3,10 +3,17 @@ import { DateSelector } from "../index";
 import { useNavigate } from "react-router-dom";
 export const FinalPrice = ({ items }) => {
   const { price, rating } = items;
-  const {dateCheckIn,dateCheckOut}=useDate()
- console.log(Number(dateCheckOut-dateCheckIn));
+  const { dateCheckIn, dateCheckOut, guests, dateDispatch } = useDate();
+
+  const handleeGuestsChange = (event) => {
+    dateDispatch({
+      type: "GUESTS",
+      payload: event.target.value,
+    });
+  };
+
   return (
-    <div className="final-price-container">
+    <div className="final-price-container gap-s">
       <div className="final-price-rate-container d-flex dir-row gap-xl ">
         <div className="final-price-cost">Rs. {price}</div>
         <div className=" d-flex dir-row align-center">
@@ -15,15 +22,57 @@ export const FinalPrice = ({ items }) => {
         </div>
       </div>
       <div className="final-price-check-typecontainer d-flex dir-row gap-xl ">
-      <div className="searcher-date-option ">
+        <div className="searcher-date-option ">
           <label className="label cursor-pointer">Check In</label>
-          <DateSelector checkInType="in"  />
+          <DateSelector checkInType="in" />
         </div>
         <div className="searcher-date-option ">
           <label className="label cursor-pointer">Check Out</label>
           <DateSelector checkInType="out" />
         </div>
-      </div>
+        </div>
+        <div className="final-price-guests">
+          <div className="guests ">
+            <p>GUESTS</p>
+            {guests <= 0 ? (
+              <input
+                className="guest-count-input"
+                type="number"
+                placeholder="Add Guests"
+                value={guests}
+                onChange={handleeGuestsChange}
+              />
+            ) : (
+              <span>{guests} guests</span>
+            )}
+          </div>
+        </div>
+
+        <button
+          className="reserve-button pointer-cursor"
+        //   onClick={handleReserveClick}
+        >
+          Reserve
+        </button>
+   
+       <div className="price-distribution d-flex dir-col ">
+        <div className="final-price d-flex align-center gap-s space-between ">
+          <span className="span">Rs. {price} x 2 Nights</span>
+          <span className="span">Rs. {price * 2}</span>
+        </div>
+        <div className="final-price d-flex align-center space-between ">
+          <span className="span">Service fee</span>
+          <span className="span">Rd. 200</span>
+        </div>
+        <div className="final-price d-flex align-center space-between">
+          <span className="span">Total</span>
+          <span className="span text-bold ">Rs. {price * 2 + 200}</span>
+        </div>  
+        </div>
+    
+     
+
+
     </div>
   );
 };
